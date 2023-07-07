42f193f4591cf931360426fd69908dc55bb1321b
unknown <milanzec53@gmail.com>
2023-07-07 18:54:36 +0200

    events binded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onButtonClick(){
-    console.log("the button was clicked")
+
+  password = ''
+  length = 0
+  includeLetters = false
+  includeNumbers = false
+  includeSymbols = false
+
+  onButtonClick() {
+    this.password = 'MY PASSWORD!!!'
+    console.log(`
+    we are about to make o password with :
+     letters ${this.includeLetters} ,
+     numbers ${this.includeNumbers} ,
+     symbols ${this.includeSymbols}
+    `)
   }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value)
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue
+    }
+  }
+
 }
